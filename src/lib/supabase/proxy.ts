b25fb612205c50116f07ac7ff9a20d7a565570f9
aashish47 <aashish27nov@gmail.com>
2026-03-19 12:48:15 +0530

    fixed redirect in proxy

--- src/lib/supabase/proxy.ts
+++ src/lib/supabase/proxy.ts
@@ -53,6 +53,17 @@ export async function updateSession(request: NextRequest) {
 		return NextResponse.redirect(url);
 	}
 
+	if (
+		(user && request.nextUrl.pathname.startsWith("/login")) ||
+		request.nextUrl.pathname.startsWith("/signup") ||
+		request.nextUrl.pathname.startsWith("/auth")
+	) {
+		// user exists and trying to reach auth routes, potentially respond by redirecting the user to root page
+		const url = request.nextUrl.clone();
+		url.pathname = "/";
+		return NextResponse.redirect(url);
+	}
+
 	// IMPORTANT: You *must* return the supabaseResponse object as it is. If you're
 	// creating a new response object with NextResponse.next() make sure to:
 	// 1. Pass the request in it, like so:
